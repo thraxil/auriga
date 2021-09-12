284317d106510f6ad4160d4c430b05a02f6ee136
Anders Pearson <anders@thraxil.org>
2021-09-12 16:35:14 +0100

    use registered users for chat instead of random usernames

--- lib/auriga_web/live/room_live.ex
+++ lib/auriga_web/live/room_live.ex
@@ -2,26 +2,36 @@ defmodule AurigaWeb.RoomLive do
   use AurigaWeb, :live_view
   require Logger
 
+  alias Auriga.Accounts
+  alias Auriga.Accounts.User
+
+  defp find_current_user(session) do
+    with user_token when not is_nil(user_token) <- session["user_token"],
+         %User{} = user <- Accounts.get_user_by_session_token(user_token),
+         do: user
+  end
+  
   @impl true
-  def mount(%{"id" => room_id}, _session, socket) do
+  def mount(%{"id" => room_id}, session, socket) do
     topic = "room:" <> room_id
-    username = MnemonicSlugs.generate_slug(2)
+    current_user = find_current_user(session)
+
     if connected?(socket) do
       AurigaWeb.Endpoint.subscribe(topic)
-      AurigaWeb.Presence.track(self(), topic, username, %{})
+      AurigaWeb.Presence.track(self(), topic, current_user.email, %{})
     end
     {:ok,
      assign(socket, room_id: room_id,
        topic: topic,
        message: "",
-       username: username,
+       current_user: current_user,
        user_list: [],
        messages: [])}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
-    message = %{uuid: UUID.uuid4(), content: message, username: socket.assigns.username}
+    message = %{uuid: UUID.uuid4(), content: message, email: socket.assigns.current_user.email}
     AurigaWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, assign(socket, message: "")}
   end
@@ -38,11 +48,11 @@ defmodule AurigaWeb.RoomLive do
   def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
     join_messages = joins
     |> Map.keys()
-    |> Enum.map(fn username -> %{type: :system, uuid: UUID.uuid4(), content: "#{username} joined"} end)
+    |> Enum.map(fn email -> %{type: :system, uuid: UUID.uuid4(), content: "#{email} joined"} end)
 
     leave_messages = leaves
     |> Map.keys()
-    |> Enum.map(fn username -> %{type: :system, uuid: UUID.uuid4(), content: "#{username} left"} end)
+    |> Enum.map(fn email -> %{type: :system, uuid: UUID.uuid4(), content: "#{email} left"} end)
 
     user_list = AurigaWeb.Presence.list(socket.assigns.topic) |> Map.keys()
     {:noreply, assign(socket, messages: socket.assigns.messages ++ join_messages ++ leave_messages,
@@ -55,9 +65,9 @@ defmodule AurigaWeb.RoomLive do
     """
   end
 
-  def display_message(%{uuid: uuid, username: username, content: content}) do
+  def display_message(%{uuid: uuid, email: email, content: content}) do
     ~E"""
-    <p><b><%= username %>:</b> <%= content %></p>
+    <p><b><%= email %>:</b> <%= content %></p>
     """
   end
 end
--- lib/auriga_web/router.ex
+++ lib/auriga_web/router.ex
@@ -21,7 +21,6 @@ defmodule AurigaWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
-    live "/chat/:id", RoomLive, :index
   end
 
   # Other scopes may use custom stacks.
@@ -66,6 +65,7 @@ defmodule AurigaWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    live "/chat/:id", RoomLive, :index
   end
 
   scope "/", AurigaWeb do
