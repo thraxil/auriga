0870f48469582d18e65d0493cba8b91fa5075cd5
Anders Pearson <anders@thraxil.org>
2023-01-01 14:02:34 +0000

    dev esbuild config

--- config/dev.exs
+++ config/dev.exs
@@ -14,20 +14,14 @@ config :auriga, Auriga.Repo,
 #
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
+# with esbuild to bundle .js and .css sources.
 config :auriga, AurigaWeb.Endpoint,
   http: [port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
 
 # ## SSL Support
