c6ec2ea8862c4562211ef16fcd47e356e0224141
Anders Pearson <anders@thraxil.org>
2026-09-08 14:35:35 +0100

    deploy fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,15 +29,8 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  maybe_direct_database_url =
-    if System.get_env("RUN_MIGRATIONS") do
-      System.get_env("DIRECT_DATABASE_URL") || database_url
-    else
-      database_url
-    end
-
   config :auriga, Auriga.Repo,
-    url: maybe_direct_database_url,
+    url: database_url,
     # IMPORTANT: Or it won't find the DB server
     socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5"),
--- lib/auriga/release.ex
+++ lib/auriga/release.ex
@@ -6,7 +6,6 @@ defmodule Auriga.Release do
   @app :auriga
 
   def migrate do
-    System.put_env("RUN_MIGRATIONS", "true")
     load_app()
 
     for repo <- repos() do
